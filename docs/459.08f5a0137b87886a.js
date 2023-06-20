ea976b77ff2b4dda6d689801aab71c345350afe7
Bubbersons <bubbersons07@gmail.com>
2023-06-21 02:48:15 +0530

    Updated Nav and Footer

---
+++ docs/459.08f5a0137b87886a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkbubbersons_angular=self.webpackChunkbubbersons_angular||[]).push([[459],{1459:(_,u,a)=>{a.r(u),a.d(u,{CategoriesModule:()=>r});var s=a(6895),e=a(7715),n=a(1571);let r=(()=>{class o{}return o.\u0275fac=function(t){return new(t||o)},o.\u0275mod=n.oAB({type:o}),o.\u0275inj=n.cJS({imports:[s.ez,e.q]}),o})()}}]);
